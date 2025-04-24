a627260836949bed29e375632f2a0b974588f709
siihasann <mashasanr@gmail.com>
2025-04-24 23:03:20 +0700

    static folder for assets

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.set('views', path.join(__dirname, 'views'));
 // Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'));     
-
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res) => {
   res.render('home', { title: 'Home' });
